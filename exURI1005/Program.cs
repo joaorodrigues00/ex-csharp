1c84849c9b9e0514b1711aa5db8399540ed4ed4d
João <joao.rodrigues0033@gmail.com>
2024-09-25 22:31:25 -0300

    exercicio 1005 aprovado

---
+++ exURI1005/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double media = (A * 3.5 + B * 7.5) / 11;
+
+        Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+
+    }
+
+}
