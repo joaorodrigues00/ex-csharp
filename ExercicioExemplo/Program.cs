88b9f60e2769bf3bad4abd8e295b128c5c7ae00f
João <joao.rodrigues0033@gmail.com>
2024-09-25 21:39:37 -0300

    primeiro commit, adicionando o gitignore e primeiro exercicio de exemplo

---
+++ ExercicioExemplo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa ?");
+            int rooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha)");
+            string[] v = Console.ReadLine().Split(' ');
+
+            string lastName = v[0];
+            int age = int.Parse(v[1]);
+            double height = double.Parse(v[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("====== Resultado ======");
+
+            Console.WriteLine($"Nome: {name}");
+            
+            Console.WriteLine($"Quartos: {rooms}");
+
+            Console.WriteLine($"Preço: {price.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine($"Ultimo Nome: {lastName}");
+
+            Console.WriteLine($"Idade: {age}");
+
+            Console.WriteLine($"Altura: {height.ToString("", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
