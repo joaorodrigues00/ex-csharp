b00d1c56ffbca13fde6584c9ab84b4098e3ab51d
João <joao.rodrigues0033@gmail.com>
2024-10-07 20:12:04 -0300

    exercicio 1008 aprovado

---
+++ exURI1008/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int employeeNumber = int.Parse(Console.ReadLine());
+        int workedHours = int.Parse(Console.ReadLine());
+
+        double valueHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double employeeSalary = workedHours * valueHour;
+
+        Console.WriteLine($"NUMBER = {employeeNumber}");
+        Console.WriteLine($"SALARY = U$ {employeeSalary.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+
+}
