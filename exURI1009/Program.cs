d924b6b7e366b5a4a61798cbee87282dc426febd
João <joao.rodrigues0033@gmail.com>
2024-10-07 20:59:16 -0300

    exercicio 1009 aprovado

---
+++ exURI1009/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string seller = Console.ReadLine();
+
+        double sellerSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double monthSales = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double comission = monthSales * 0.15;
+
+        double total = sellerSalary + comission;
+
+        Console.WriteLine($"TOTAL = R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+
+}
