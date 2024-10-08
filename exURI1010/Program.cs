76c4e41c6a8939d2fa7cf7a674606fde15042fb8
João <joao.rodrigues0033@gmail.com>
2024-10-07 21:30:09 -0300

    exercicio 1010 aprovado

---
+++ exURI1010/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] product1 = Console.ReadLine().Split(' ');
+
+        string[] product2 = Console.ReadLine().Split(' ');
+
+        int p1Code = int.Parse(product1[0]);
+
+        int p1PieceQuantity = int.Parse(product1[1]);
+
+        double p1PieceValue = double.Parse(product1[2], CultureInfo.InvariantCulture);
+
+        int p2Code = int.Parse(product2[0]);
+
+        int p2PieceQuantity = int.Parse(product2[1]);
+
+        double p2PieceValue = double.Parse(product2[2], CultureInfo.InvariantCulture);
+
+        double total = p1PieceQuantity * p1PieceValue + p2PieceQuantity * p2PieceValue;
+
+        Console.WriteLine($"VALOR A PAGAR: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+
+    }
+
+}
