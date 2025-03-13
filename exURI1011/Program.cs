b9144010fff9f62a4a338f0b1c6b6eb3ae6847fd
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-03-12 22:10:41 -0300

    exercicio 1011 aprovado

---
+++ exURI1011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        var pi = 3.14159;
+
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        var formula = (4.0/3) * pi * Math.Pow(raio, 3);
+
+        Console.WriteLine($"VOLUME = {formula.ToString("F3", CultureInfo.InvariantCulture)}");
+
+    }
+
+}
