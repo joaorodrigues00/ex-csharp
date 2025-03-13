e372274c7d3f12995e62d9a36cf8d7dfe87839ba
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-03-12 23:10:31 -0300

    exercicio 1014 aprovado

---
+++ exURI1014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        var consumoMedio = x / y;
+
+        Console.WriteLine($"{consumoMedio.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+    }
+
+}
