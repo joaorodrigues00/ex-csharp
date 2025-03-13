4df92fc3d9e2d12ea170af91fbaa4a80a1301226
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-03-12 22:49:35 -0300

    exercicio 1013 aprovado

---
+++ exURI1013/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] values = Console.ReadLine().Split(' ');
+
+        int A = int.Parse(values[0]);
+        int B = int.Parse(values[1]);
+        int C = int.Parse(values[2]);
+
+        int maiorAb = (A + B + Math.Abs(A - B)) / 2;
+
+        if (maiorAb > C)
+        {
+            Console.WriteLine($"{maiorAb} eh o maior");
+        }
+        else
+        {
+            Console.WriteLine($"{C} eh o maior");
+        }
+    }
+
+}
