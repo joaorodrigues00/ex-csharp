98a3fd0de94451cd58323d33e7520dc2e90e8d39
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-03-25 22:10:51 -0300

    exercicio 1015 aprovado

---
+++ exURI1015/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string[] point1 = Console.ReadLine().Split(' ');
+
+        string[] point2 = Console.ReadLine().Split(' ');
+
+        double x1 = double.Parse(point1[0], CultureInfo.InvariantCulture);
+
+        double y1 = double.Parse(point1[1], CultureInfo.InvariantCulture);
+
+        double x2 = double.Parse(point2[0], CultureInfo.InvariantCulture);
+
+        double y2 = double.Parse(point2[1], CultureInfo.InvariantCulture);
+
+        double result = Math.Sqrt((Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)));
+
+        Console.WriteLine($"{result.ToString("F4", CultureInfo.InvariantCulture)}");
+    }
+
+}
