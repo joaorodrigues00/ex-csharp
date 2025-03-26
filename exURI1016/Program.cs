be767990fdea5886659797209ca13cd01ff71095
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-03-25 22:58:37 -0300

    exercicio 1016 aprovado

---
+++ exURI1016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int distance = int.Parse(Console.ReadLine());
+
+        int result = distance * 2;
+
+        Console.WriteLine($"{result} minutos");
+
+    }
+
+}
