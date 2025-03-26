328c5f53703cda7bb3298b879bd419580cf4b31b
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-03-25 23:11:45 -0300

    exercicio 1017 aprovado

---
+++ exURI1017/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double time = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double velocity = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double result = time * velocity / 12;
+
+        Console.WriteLine($"{result.ToString("F3", CultureInfo.InvariantCulture)}");
+    }
+
+}
