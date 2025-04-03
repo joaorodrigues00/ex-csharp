0620382814e734caad37b4586863cfae2fcc7a5f
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-04-02 23:14:26 -0300

    exercicio 1020 aprovado

---
+++ exURI1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int value = int.Parse(Console.ReadLine());
+
+        int yearMod = value % 365;
+        int yearDiv = value / 365;
+
+        int monthMod = yearMod % 30;
+        int monthDiv = yearMod / 30;
+
+        int daysMod = monthMod;
+
+        Console.WriteLine($"{yearDiv} ano(s)");
+        Console.WriteLine($"{monthDiv} mes(es)");
+        Console.WriteLine($"{daysMod} dia(s)");
+    }
+
+}
