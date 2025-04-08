43af65b3e8c6f4e9daf645f5b44e46a6571d445d
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-04-08 20:54:40 -0300

    execicio alfabeto

---
+++ alfabeto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Alfabeto
+{
+    static void Main(string[] args)
+    {
+
+        string[] alfabeto = [
+            "A", "B", "C", "D", "E", "F",
+            "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X",
+            "Y", "Z"
+        ];
+
+        int letra = int.Parse(Console.ReadLine());
+        int counter = 0;
+
+        while (letra >= counter)
+        {
+            Console.WriteLine($"{alfabeto[counter]}");
+            counter++;
+        }
+    }
+}
