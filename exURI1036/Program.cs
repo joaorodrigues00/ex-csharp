05fec2151b20da64242e747050abfcd04130a973
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-05-06 11:00:16 -0300

    exercicio 1036 aprovado

---
+++ exURI1036/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] values = Console.ReadLine().Split(' ');
+
+        double a, b, c, delta, r1, r2;
+
+        a = double.Parse(values[0], CultureInfo.InvariantCulture);
+        b = double.Parse(values[1], CultureInfo.InvariantCulture);
+        c = double.Parse(values[2], CultureInfo.InvariantCulture);
+
+        delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+        if (a == 0 || delta < 0.0)
+        {
+            Console.WriteLine("Impossivel calcular");
+        }
+        else
+        {
+            r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine($"R1 = {r1.ToString("F5", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"R2 = {r2.ToString("F5", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
