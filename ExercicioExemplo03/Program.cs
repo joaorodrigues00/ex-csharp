f093c7db44242d891c123ecf90f21938cdd903f0
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-05-14 09:36:27 -0300

    exercicio exemplo 03

---
+++ ExercicioExemplo03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Program {
+    public static void Main(string[] args) {
+
+        double basicPlan = 50.00;
+
+        int minutes = int.Parse(Console.ReadLine());
+
+        if (minutes < 100) {
+            Console.WriteLine($"Valor a pagar: R$ {basicPlan.ToString("F2", CultureInfo.InvariantCulture)}");
+        } else {
+            basicPlan += (minutes - 100) * 2.0;
+
+            Console.WriteLine($"Valor a pagar: R$ {basicPlan.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
