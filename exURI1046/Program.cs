536341c5ca019fb6dfd13111883cc7bd09663b8c
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-07-08 22:09:45 -0300

    exercicio aprovado 1046

---
+++ exURI1046/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        string[] gameHours = Console.ReadLine().Split(' ');
+
+        int firstHour = int.Parse(gameHours[0]);
+        int lastHour = int.Parse(gameHours[1]);
+
+        int result;
+
+        if (firstHour < lastHour)
+        {
+            result = lastHour - firstHour;
+        }
+        else
+        {
+            result = 24 - firstHour + lastHour;
+        }
+
+        Console.WriteLine($"O JOGO DUROU {result} HORA(S)");
+    }
+
+}
