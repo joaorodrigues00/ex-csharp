d5adfadd2cb9960d8b07592e7abecf0d124cb8f5
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-07-08 21:20:40 -0300

    exercicio 1044 aprovado

---
+++ exURI1044/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        string[] multiples = Console.ReadLine().Split(' ');
+
+        int n1 = int.Parse(multiples[0]);
+
+        int n2 = int.Parse(multiples[1]);
+
+        int div = n2 % n1;
+
+        if (n1 > n2)
+        {
+            div = n1 % n2;
+        }
+
+        if (div == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+    }
+
+}
