89856e4f21e8b95f94e177e900f3933c76dafed4
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-08-20 17:07:54 -0300

    exercicio 1037 aprovado

---
+++ exURI1037/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        double intervalNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if (intervalNumber >= 0.0 && intervalNumber <= 25.0)
+        {
+            Console.WriteLine("Intervalo [0,25]");
+        }
+        else if (intervalNumber >= 25.0 && intervalNumber <= 50.0)
+        {
+            Console.WriteLine("Intervalo (25,50]");
+        }
+        else if (intervalNumber >= 50.0 && intervalNumber <= 75.0)
+        {
+            Console.WriteLine("Intervalo (50,75]");
+        }
+        else if (intervalNumber >= 75.0 && intervalNumber <= 100.0)
+        {
+            Console.WriteLine("Intervalo (75,100]");
+        }
+        else
+        {
+            Console.WriteLine("Fora de intervalo");
+        }
+    }
+
+}
