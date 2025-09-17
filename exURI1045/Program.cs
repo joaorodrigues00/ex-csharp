7ae6861ca5f7171e555b749b5980fcd73ce715e7
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-09-16 21:10:20 -0300

    exercicio 1045 aprovado

---
+++ exURI1045/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        string[] triangulos = Console.ReadLine().Split(' ');
+
+        double n1 = double.Parse(triangulos[0], CultureInfo.InvariantCulture);
+        double n2 = double.Parse(triangulos[1], CultureInfo.InvariantCulture);
+        double n3 = double.Parse(triangulos[2], CultureInfo.InvariantCulture);
+
+        double A, B, C;
+
+        if (n1 > n2 && n1 > n3)
+        {
+            A = n1;
+            if (n2 > n3)
+            {
+                B = n2;
+                C = n3;
+            }
+            else
+            {
+                B = n3;
+                C = n2;
+            }
+        }
+        else if (n2 > n3)
+        {
+            A = n2;
+            if (n1 > n3)
+            {
+                B = n1;
+                C = n3;
+            }
+            else
+            {
+                B = n3;
+                C = n1;
+            }
+        }
+        else
+        {
+            A = n3;
+
+            if (n1 > n2)
+            {
+                B = n1;
+                C = n2;
+            }
+            else
+            {
+                B = n2;
+                C = n1;
+            }
+        }
+
+        if (A >= B + C)
+        {
+            Console.WriteLine("NAO FORMA TRIANGULO");
+        }
+        else
+        {
+         if (A * A == B * B + C * C)
+        {
+            Console.WriteLine("TRIANGULO RETANGULO");
+        }
+        if (A * A > B * B + C * C)
+        {
+            Console.WriteLine("TRIANGULO OBTUSANGULO");
+        }
+        if (A * A < B * B + C * C)
+        {
+            Console.WriteLine("TRIANGULO ACUTANGULO");
+        }
+        
+        if (A == B && B == C)
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO");
+            }
+        if (
+            (A == B && A != C) ||
+            (B == C && B != A) ||
+            (A == C && A != B)
+        )
+        {
+           Console.WriteLine("TRIANGULO ISOSCELES"); 
+        }   
+        }
+
+    }
+
+}
