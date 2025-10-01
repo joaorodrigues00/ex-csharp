7c462f4ec0db379a1b7fcabdbcccc449e816338e
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-09-30 21:52:04 -0300

    exercicio 1049 aprovado

---
+++ exURI1049/Program.cs
@@ -0,0 +1,67 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        var word1 = Console.ReadLine();
+        var word2 = Console.ReadLine();
+        var word3 = Console.ReadLine();
+        var result = "";
+
+        if (word1 == "vertebrado")
+        {
+            if (word2 == "ave")
+            {
+                if (word3 == "carnivoro")
+                {
+                    result = "aguia";
+                }
+                else
+                {
+                    result = "pomba";
+                }
+            }
+            else
+            {
+                if (word3 == "onivoro")
+                {
+                    result = "homem";
+                }
+                else
+                {
+                    result = "vaca";
+                }
+            }
+        }
+        else
+        {
+            if (word2 == "inseto")
+            {
+                if (word3 == "hematofago")
+                {
+                    result = "pulga";
+                }
+                else
+                {
+                    result = "lagarta";
+                }
+            }
+            else
+            {
+                if (word3 == "hematofago")
+                {
+                    result = "sanguessuga";
+                }
+                else
+                {
+                    result = "minhoca";
+                }
+            }
+        }
+
+        Console.WriteLine($"{result}");
+    }
+
+}
