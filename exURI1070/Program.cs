22d90f1a646ed842e1f89fedaf254ad1e8533c69
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-06 23:01:32 -0300

    exercicio 1070 aprovado

---
+++ exURI1070/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        int positiveNumber = int.Parse(Console.ReadLine());
+
+        int oddCounter = 0;
+
+        while (oddCounter < 6)
+        {
+            if (positiveNumber % 2 != 0)
+            {
+                oddCounter++;
+                Console.WriteLine(positiveNumber);
+            }
+
+            positiveNumber++;
+        }
+
+    }
+
+}
