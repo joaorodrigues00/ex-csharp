5ecd1cf718ec72758a59d590555716aa4751802f
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-06 22:29:47 -0300

    exercicio 1065 aprovado

---
+++ exURI1065/Program.cs
@@ -0,0 +1,44 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args)
+    {
+
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+        int n4 = int.Parse(Console.ReadLine());
+        int n5 = int.Parse(Console.ReadLine());
+
+        int parCounter = 0;
+
+        if (n1 % 2 == 0)
+        {
+            parCounter++;
+        }
+
+        if (n2 % 2 == 0)
+        {
+            parCounter++;
+        }
+
+        if (n3 % 2 == 0)
+        {
+            parCounter++;
+        }
+
+        if (n4 % 2 == 0)
+        {
+            parCounter++;
+        }
+
+        if (n5 % 2 == 0)
+        {
+            parCounter++;
+        }
+
+        Console.WriteLine($"{parCounter} valores pares");
+    }
+
+}
