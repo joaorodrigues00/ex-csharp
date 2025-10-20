b2a82c03c2816904017da90ee0400da878abfedf
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-20 20:47:25 -0300

    exercicio 1113 aprovado

---
+++ exURI1113/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string[] vet = Console.ReadLine().Split(' ');
+        int x = int.Parse(vet[0]);
+        int y = int.Parse(vet[1]);
+
+        while (x != y)
+        {
+            if (x > y)
+            {
+                Console.WriteLine("Decrescente");
+            }
+            else
+            {
+                Console.WriteLine("Crescente");
+            }
+
+            vet = Console.ReadLine().Split(' ');
+            
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+        }
+
+    }
+
+}
