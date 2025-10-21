50c5824f894ed42da72d82bc78fafad0938b11df
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-20 21:06:48 -0300

    exercicio exemplo while

---
+++ ExercicioExemplo06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+class URI {
+
+    static void Main(string[] args) {
+
+        double age = double.Parse(Console.ReadLine());
+        double ageMedia = 0;
+        int count = 0;
+
+        if (age < 0)
+        {
+            Console.WriteLine("impossivel calcular");
+        } else
+        {
+            while (age > 0)
+            {
+                ageMedia = ageMedia + age;
+                count++;
+
+                age = int.Parse(Console.ReadLine());
+            }
+
+            double result = ageMedia / count;
+
+            Console.WriteLine(result.ToString("F2", CultureInfo.InvariantCulture));   
+        }
+    }
+
+}
