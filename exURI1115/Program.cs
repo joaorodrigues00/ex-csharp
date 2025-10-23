381b693b01e1b98f5769cfd8915b5c4226b72fdf
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-23 13:03:12 -0300

    exercicio 1115 aprovado

---
+++ exURI1115/Program.cs
@@ -0,0 +1,38 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) {
+
+        string[] vet = Console.ReadLine().Split(' ');
+        int x = int.Parse(vet[0]);
+        int y = int.Parse(vet[1]);
+
+        while(x != 0 && y != 0)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("primeiro");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("segundo");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("terceiro");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("quarto");
+            }
+
+            vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+        }
+
+    }
+
+}
