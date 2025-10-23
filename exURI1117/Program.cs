7d0f620b76cd908e526ab41466c9398524c60804
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-23 13:41:12 -0300

    exercicio 1117 aprovado

---
+++ exURI1117/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double note = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        int validNote = 0;
+        double result = 0;
+
+        while (validNote < 2)
+        {
+            if (note < 0.0 || note > 10.0)
+            {
+                Console.WriteLine("nota invalida");
+            }
+            else
+            {
+                result = result + note;
+                validNote++;
+            }
+
+            if (validNote < 2)
+            {
+                note = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+
+        double finalResult = result / 2.0;
+
+        Console.WriteLine($"media = {finalResult.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
