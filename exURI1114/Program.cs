7fd09ea6fe23b70bb507e01f21a0876b271675fc
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-23 12:28:58 -0300

    exercicio 1114 aprovado

---
+++ exURI1114/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int password = int.Parse(Console.ReadLine());
+
+        while (password != 2002)
+        {
+            Console.WriteLine("Senha Invalida");
+
+            password = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Acesso Permitido");
+    }
+
+}
