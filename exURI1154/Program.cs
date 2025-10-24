8c3b851d9f50918e8c6d73fd1b219f6e4f9a2a10
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-23 22:54:34 -0300

    exercicio 1154 aprovado

---
+++ exURI1154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double ages = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        int ageCounter = 0;
+        double ageResults = 0;
+
+        while (ages > 0)
+        {
+            ageResults += ages;
+            ageCounter++;
+            ages = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        double avgAge = ageResults / ageCounter;
+        Console.WriteLine($"{avgAge.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
