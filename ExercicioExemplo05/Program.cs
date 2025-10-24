c111d834c611dbe432060904130a427b583180ba
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-23 22:37:46 -0300

    exercicio exemplo while

---
+++ ExercicioExemplo05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+class Curso {
+
+    static void Main(string[] args)
+    {
+        string[] nArray = Console.ReadLine().Split(' ');
+
+        int count = 0;
+        int itensSum = 0;
+
+        while (int.Parse(nArray[count]) > 0)
+        {
+            itensSum = itensSum + int.Parse(nArray[count]);
+            count++;
+        }
+
+        Console.WriteLine($"Soma final {itensSum}");
+    }
+
+}
