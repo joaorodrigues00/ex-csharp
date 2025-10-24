e565012b7a928d36316c93947a9ec6d2dcb599dd
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-23 22:46:25 -0300

    exercicio 1134 aprovado

---
+++ exURI1134/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int code = int.Parse(Console.ReadLine());
+
+        int alcohol = 0;
+        int gasoline = 0;
+        int diesel = 0;
+
+        while (code != 4)
+        {
+            switch (code)
+            {
+                case 1:
+                    alcohol++;
+                    break;
+                case 2:
+                    gasoline++;
+                    break;
+                case 3:
+                    diesel++;
+                    break;
+            }
+
+            code = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("MUITO OBRIGADO");
+
+        Console.WriteLine($"Alcool: {alcohol}");
+        Console.WriteLine($"Gasolina: {gasoline}");
+        Console.WriteLine($"Diesel: {diesel}");
+    }
+}
