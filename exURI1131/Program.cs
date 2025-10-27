e8241b4f19933696856cb8046281527aac4ab1d5
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-27 20:16:26 -0300

    exercicio 1131 aprovado

---
+++ exURI1131/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int grenalOption = 1;
+        int grenalQuantity = 0;
+        int grenalDrawQuantity = 0;
+        int interWin = 0;
+        int gremioWin = 0;
+
+        while (grenalOption != 2)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int interGoals = int.Parse(vet[0]);
+            int gremioGoals = int.Parse(vet[1]);
+
+            if (interGoals == gremioGoals)
+            {
+                grenalDrawQuantity++;
+            }
+
+            if (interGoals > gremioGoals)
+            {
+                interWin++;
+            }
+            else
+            {
+                gremioWin++;
+            }
+
+            grenalQuantity++;
+
+            Console.WriteLine("Novo grenal (1-sim 2-nao)");
+            grenalOption = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"{grenalQuantity} grenais");
+        Console.WriteLine($"Inter:{interWin}");
+        Console.WriteLine($"Gremio:{gremioWin}");
+        Console.WriteLine($"Empates:{grenalDrawQuantity}");
+
+        if (interWin == gremioWin)
+        {
+            Console.WriteLine("Nao houve vencedor");
+        } else if (interWin > gremioWin)
+        {
+            Console.WriteLine("Inter venceu mais");
+        } else
+        {
+            Console.WriteLine("Gremio venceu mais");
+        }
+    }
+}
