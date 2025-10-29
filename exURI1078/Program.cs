e7bc91309dc40c5816cf9878ff4195f11039bdec
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-28 22:11:42 -0300

    exercicio 1078 aprovado

---
+++ exURI1078/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine($"{i} x {N} = {i * N}");
+        }
+    }
+}
