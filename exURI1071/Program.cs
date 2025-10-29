f112194c68f30d709eb983f199afea6331d0b61b
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-10-28 22:40:26 -0300

    exercicio 1071 aprovado

---
+++ exURI1071/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+        int min, max;
+        int soma = 0;
+
+        if (x < y)
+        {
+            min = x;
+            max = y;
+        } else
+        {
+            min = y;
+            max = x;
+        }
+
+        for (int i = min + 1; i < max; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma = soma + i;
+            }
+        }
+
+        Console.WriteLine(soma);
+    }
+}
