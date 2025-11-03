333249bf1e9d82741ebdc4b031c2c1a303132b28
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-11-03 19:47:14 -0300

    exercicio 1074 aprovado

---
+++ exURI1074/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string oddEven;
+        string minusPlus;
+
+        for (int i = 0; i < N; i++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if (X > 0)
+            {
+                minusPlus = "POSITIVE";
+            }
+            else
+            {
+                minusPlus = "NEGATIVE";
+            }
+            
+            if (X % 2 == 0)
+            {
+                oddEven = "EVEN";
+            } else
+            {
+                oddEven = "ODD";
+            }
+
+            if (X == 0)
+            {
+                Console.WriteLine("NULL");
+            } else
+            {
+                Console.WriteLine($"{oddEven} {minusPlus}");   
+            }
+        }
+    }
+}
