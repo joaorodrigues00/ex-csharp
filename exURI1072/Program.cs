160d0d15512eefa9a5c6a603e62e3b26e5b0e732
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-11-03 19:16:19 -0300

    exercicio 1072 aprovado

---
+++ exURI1072/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int inInterval = 0;
+        int outInterval = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if (X >= 10 && X <= 20)
+            {
+                inInterval++;
+            }
+            else
+            {
+                outInterval++;
+            }
+        }
+
+        Console.WriteLine($"{inInterval} in");
+        Console.WriteLine($"{outInterval} out");
+    }
+}
