42d0ebdfea97333ca3d65ec3bc29285d27d24962
João Rodrigues <joao.rodrigues0033@gmail.com>
2025-12-18 19:23:16 -0300

    exercicio 1080 aprovado

---
+++ exURI1080/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int greaterN = int.Parse(Console.ReadLine());
+        int positionN = 1;
+
+        for (int i = 2; i <= 100; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > greaterN)
+            {
+                greaterN = n;
+                positionN = i;
+            }
+        }
+
+        Console.WriteLine($"{greaterN}");
+        Console.WriteLine($"{positionN}");
+
+    }
+
+}
