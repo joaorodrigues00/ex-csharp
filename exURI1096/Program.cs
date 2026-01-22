6e24035504db15564109aba98d9933aa9a016b4b
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-01-21 22:46:39 -0300

    exercicio 1096 aprovado

---
+++ exURI1096/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 7; j > 4; j--)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine($"I={i} J={j}");   
+                }
+            }
+        }
+    }
+}
