8977cce82a1a646c3fa532a07e42acd2b801d287
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-11 20:50:06 -0300

    exercicio 1116 aprovado

---
+++ exURI1116/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class URI
+{
+    static void Main(string[] args)
+    {
+        int operationAmount;
+        double dividendo, divisor, result;
+
+        operationAmount = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < operationAmount; i ++)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            dividendo = double.Parse(vet[0]);
+            divisor = double.Parse(vet[1]);
+
+            result = dividendo / divisor;
+
+            if (divisor == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            } else
+            {
+                Console.WriteLine(result.ToString("F1"));
+            }
+        }
+    }
+}
