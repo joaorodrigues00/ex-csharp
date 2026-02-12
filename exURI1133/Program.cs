7205ffa8d39ffe9d2dab5b16e7882b5a381bc1b6
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-11 22:08:38 -0300

    exercicio 1133 aprovado

---
+++ exURI1133/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class URI
+{
+    static void Main(string[] args)
+    {
+        int x, y;
+
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        int minValue = Math.Min(x, y);
+        int maxValue = Math.Max(x, y);
+
+        for (int i = minValue+1; i < maxValue; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
