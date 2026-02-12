2b9d161f9e412f01f6825346ed31885889c58c99
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-11 22:30:23 -0300

    exercicio 1142 aprovado

---
+++ exURI1142/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class URI
+{
+    static void Main(string[] args)
+    {
+        int PUM = int.Parse(Console.ReadLine());
+        int value = 1;
+
+        for (int i = 1; i <= PUM; i++)
+        {
+            Console.WriteLine($"{value} {value + 1} {value + 2} PUM");
+            value += 4;
+        }
+    }
+}
