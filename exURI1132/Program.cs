6df0998d7ebd58cce18198c40a24a69aa9703f96
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-11 21:55:12 -0300

    exercicio 1132 aprovado

---
+++ exURI1132/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class URI
+{
+    static void Main(string[] args)
+    {
+        int x, y, initialIterationValue, lastIterationValue;
+        int result = 0;
+
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        if (x < y)
+        {
+            initialIterationValue = x;
+            lastIterationValue = y;
+        } else
+        {
+            initialIterationValue = y;
+            lastIterationValue = x;
+        }
+
+        for (int i = initialIterationValue; i <= lastIterationValue; i++)
+        {
+            if (i % 13 != 0)
+            {
+                result += i ;
+            }
+        }
+
+        Console.WriteLine($"{result}");
+    }
+}
