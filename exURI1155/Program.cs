5117ec046f31dc688993c807f2393c300599e5da
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-16 13:19:23 -0300

    exercicio 1155 aprovado

---
+++ exURI1155/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double s = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            s = s + 1.0 / i;
+        }
+
+        Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+    }
+
+}
