294b89c6d8326dea3546f9490d3acbc8cb144eab
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-16 13:44:35 -0300

    exercicio 1156 aprovado

---
+++ exURI1156/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        double s = 0.0;
+        double j = 1.0;
+        for (int i = 1; i <= 39; i += 2)
+        {
+            s  = s + i / j;
+            j = j * 2.0;
+        }
+
+        Console.WriteLine($"{s.ToString("F2")}");
+
+    }
+
+}
