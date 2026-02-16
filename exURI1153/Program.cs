96762d9c65dad62b545161ffd2ced7fa39775102
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-16 13:08:30 -0300

    exercicio 1153 aprovado

---
+++ exURI1153/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class URI
+{
+    static void Main(string[] args)
+    {
+
+        int N = int.Parse(Console.ReadLine());
+        int fat = 1;
+
+        for (int i = N; i > 0; i--)
+        {
+            fat = fat * i;
+        }
+
+        Console.WriteLine($"{fat}");
+    }
+}
