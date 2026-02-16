3ce19ad1a3e2f78980741ead068ea2a0515c49c9
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-02-16 11:27:08 -0300

    exercicio 1151 aprovado

---
+++ exURI1151/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        int atual = 0;
+        int anterior = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+            {
+                Console.WriteLine(atual);
+            }
+            else
+            {
+                Console.Write($"{atual} ");
+            }
+
+            int aux = anterior;
+            anterior = atual;
+            atual = aux + anterior;
+        }
+    }
+
+}
