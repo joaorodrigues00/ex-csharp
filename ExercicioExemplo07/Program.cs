6ad81a4c1c486288187082030757f31f1a2703a6
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-09 23:51:07 -0300

    exercicio exemplo vetores

---
+++ ExercicioExemplo07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        float[] vet;
+        vet = new float[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            float Nd = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vet[i] = Nd;
+        }
+
+        for(int j = 0; j < vet.Length; j++)
+        {
+            Console.WriteLine($"{vet[j]:F1}");
+        }
+    }
+}
