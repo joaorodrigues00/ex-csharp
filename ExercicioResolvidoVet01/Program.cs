7086144094b128acbd55022ec5e678f9cc689fb3
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-09 23:52:39 -0300

    exercicio exemplo resolvido 01

---
+++ ExercicioResolvidoVet01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] vet1 = new int[N];
+
+        string[] s = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            vet1[i] = int.Parse(s[i]);
+            
+            if (vet1[i] < 0)
+            {
+                Console.WriteLine($"{vet1[i]}");
+            }
+        }
+    }
+}
