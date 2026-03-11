b53aa05bbbd64fd75ca542712ba501c96b0a4491
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-11 19:40:50 -0300

    exercicio Vet07 resolvido

---
+++ ExercicioVet07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+class ExampleVet07
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        double[] firstSem = new double[N];
+        double[] secondSem = new double[N];
+        double[] media = new double[N];
+        string[] names = new string[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            names[i] = s[0];
+            firstSem[i] = double.Parse(s[1], CultureInfo.InvariantCulture);
+            secondSem[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+
+            media[i] = (firstSem[i] + secondSem[i]) / 2.0;
+        }
+
+        Console.WriteLine("Alunos aprovados: ");
+
+        for (int i = 0; i < N; i++)
+        {
+            if (media[i] >= 6.0)
+            {
+                Console.WriteLine($"{names[i]}");
+            }
+        }
+
+    }
+}
