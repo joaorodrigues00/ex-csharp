12a4ca889941210cd014a0ab42177740c7542c8a
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-10 21:56:28 -0300

    Exercicio Vet03 aprovado

---
+++ ExercicioVet03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+class ExampleVet03
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] A = new int[N];
+        int[] B = new int[N];
+        int[] C = new int[N];
+
+        string[] sA = Console.ReadLine().Split(' ');
+        string[] sB = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = int.Parse(sA[i]);
+            B[i] = int.Parse(sB[i]);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            C[i] = A[i] + B[i];
+        }
+
+        foreach(int c in C)
+        {
+            Console.Write($"{c} ");
+        }
+    }
+}
