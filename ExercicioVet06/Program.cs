0ea8f35450190e6fbb0d38e5ddad41ef04510efd
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-10 23:29:13 -0300

    Exercicio Vet06 resolvido

---
+++ ExercicioVet06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class ExampleVet06
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] names = new string[N];
+        int[] ages = new int[N];
+        int oldestPerson = 0;
+        string oldestPersonName = "";
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+
+            names[i] = s[0];
+            ages[i] = int.Parse(s[1]);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            if (ages[i] > oldestPerson)
+            {
+                oldestPerson = ages[i];
+                oldestPersonName = names[i];
+            }
+        }
+
+        Console.WriteLine($"Pessoa mais Velha: {oldestPersonName}");
+    }
+}
