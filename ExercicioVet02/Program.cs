20a5bdf86f69e6ac01b53dffde386e3315174877
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-10 21:44:33 -0300

    Exercicio Vet02 resolvido

---
+++ ExercicioVet02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ExampleVet02
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] vet = new int[N];
+        int evenNumbersAmount = 0;
+
+        string[] s = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            vet[i] = int.Parse(s[i]);
+
+            if ((vet[i] & 1) == 0)
+            {
+                Console.Write($"{vet[i]} ");
+                evenNumbersAmount++;
+            }
+        }
+        Console.WriteLine();
+        Console.Write(evenNumbersAmount);
+    }
+}
