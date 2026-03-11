14fe3fb8ab2696b6f003c0a2d6ffd72c82b06a06
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-03-10 23:18:52 -0300

    Exercicio Vet05 resolvido

---
+++ ExercicioVet05/Program.cs
@@ -0,0 +1,25 @@
+﻿class ExampleVet05
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] vet = new int[N];
+        int media = 0;
+        int evenNumbers = 0;
+
+        string[] s = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++)
+        {
+            vet[i] = int.Parse(s[i]);
+
+            if ((vet[i] & 1) == 0)
+            {
+                media += vet[i];
+                evenNumbers++;
+            }
+        }
+
+        Console.WriteLine($"{media / evenNumbers:F1}");
+    }
+}
