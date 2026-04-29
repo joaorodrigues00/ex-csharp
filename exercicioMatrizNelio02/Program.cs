fa602d69b0140ffc02992b2e3b9e0437c68a4784
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-04-28 21:02:23 -0300

    exercicio nelio 02 resolvido

---
+++ exercicioMatrizNelio02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int matriceSize = int.Parse(Console.ReadLine());
+
+        int[,] matrice = new int[matriceSize, matriceSize];
+        int[] finalVet = new int[matriceSize];
+
+        for (int i = 0; i < matriceSize; i++)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            for (int j = 0; j < matriceSize; j++)
+            {
+                matrice[i, j] = int.Parse(vet[j]);
+            }
+        }
+
+        for (int i = 0; i < matriceSize; i++)
+        {
+            for (int j = 0; j < matriceSize; j++)
+            {
+                finalVet[i] += matrice[i, j];
+            }
+        }
+
+        foreach(int m in finalVet)
+        {
+            Console.WriteLine($"{m}");
+        }
+    }
+}
