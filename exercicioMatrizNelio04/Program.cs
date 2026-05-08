c53a81dc47cf9ea1306847be53c83fb1be9920ab
João Rodrigues <joao.rodrigues0033@gmail.com>
2026-05-07 22:24:55 -0300

    exercicio matriz 04 nelio

---
+++ exercicioMatrizNelio04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int[,] matrice = new int[N, N];
+        string[] vet;
+        int sum = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            vet = Console.ReadLine().Split(' ');
+            for (int j = 0; j < N; j++)
+            {
+                matrice[i, j] = int.Parse(vet[j]);
+            }
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (i < j)
+                {
+                    sum += matrice[i, j];
+                }
+            }
+        }
+        Console.WriteLine($"{sum}");
+    }
+}
